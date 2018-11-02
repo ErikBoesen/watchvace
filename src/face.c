13d7c43a35937d6cce27dbb62c31dccf97180bc3
ErikBoesen <me@erikboesen.com>
2018-11-01 20:03:47 -0400

    Write time into window

--- src/face.c
+++ src/face.c
@@ -27,8 +27,22 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
 
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void init() {
@@ -46,6 +60,9 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Make sure the time is displayed from the start
+  update_time();
 }
 
 static void deinit() {
